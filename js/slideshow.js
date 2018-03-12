740a915eb03cb69baec02044bc8dfa6d3d8d614a
richard <richardribucanp@gmail.com>
2018-03-11 20:32:42 -1000

    got the pages on

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,5 +1,8 @@
+console.log("loaded slideshow")
+
 var slideIndex = 1;
 showDivs(slideIndex);
+var back = ["./img/slide1.jpg", "./img/slide2.jpg", "./img/slide3.jpg"]
 
 function plusDivs(n) {
   showDivs(slideIndex += n);
@@ -7,11 +10,16 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
+//  var b;
   var x = document.getElementsByClassName("storyslide");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
+//  for (b=0;b>=3;b++){
+//  	console.log(b)
+//  	document.getElementById("slideshow").style.backgroundImage = "url" + "(" + back[b] + ")"
+//  }
   x[slideIndex-1].style.display = "block";  
 }
