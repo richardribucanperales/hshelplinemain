9b3e967f58b301cf7929c55d98802145e5df4812
richard <richardribucanp@gmail.com>
2018-04-06 14:24:17 -1000

    updated statistics

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # hshelplinemain
+
+What is this?
+=============
+This website was created with the delicate topic of suicide in mind. It is an anti-suicide website, designed to give people a chance to a better life with resources provided by the community. The project was introduced by Taylor Fujimoto, an activist and leader in our community.
+
+Who does this affect?
+=====================
+The students of Waipahu High School
+
+
+
--- js/herochange.js
+++ js/herochange.js
@@ -1,14 +1,11 @@
 console.log("loaded heroimage change")
-var back = ["./img/hero1.jpg", "./img/hero2.jpg", "./img/hero3.jpg"];
-
-var a = document.getElementById("hero");
-
-
-var y = Math.floor(Math.random()*3)
-console.log(y)
-
-var z = back[y]
-
-console.log(z)
-
-var x = a.style.backgroundImage = "url" + "(" + z + ")"
+function heroChange(){
+    var back = ["./img/hero1.jpg", "./img/hero2.jpg", "./img/hero3.jpg"];
+    var a = document.getElementById("hero");
+    var y = Math.floor(Math.random()*3)
+    console.log(y)
+    var z = back[y]
+    console.log(z)
+    var x = a.style.backgroundImage = "url" + "(" + z + ")"
+}
+heroChange()
